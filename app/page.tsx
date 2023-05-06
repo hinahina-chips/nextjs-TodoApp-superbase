204b40563bdd37fbbb73b2f4db7fc7754225b5d6
hinahina-chips <hinahina-chips@outlook.jp>
2023-05-07 00:05:57 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import supabase from '../utils/supabase';
-import { InputTodoForm } from '../components/InputTodoForm';
+import { InputTodoForm } from '../components/InputToDoForm';
 import { TodoList } from '../components/ToDoList';
 
 const Home : React.FC = () => {
